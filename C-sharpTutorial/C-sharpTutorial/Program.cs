116a4add956d178625845243f9b5bfa5322001e2
vietcuongk99 <daoquocthai8@gmail.com>
2020-06-30 09:51:09 +0700

    new commit

--- C-sharpTutorial/C-sharpTutorial/Program.cs
+++ C-sharpTutorial/C-sharpTutorial/Program.cs
@@ -10,19 +10,23 @@ namespace C_sharpTutorial
             int num1 = Convert.ToInt32(Console.ReadLine());
             int num2 = Convert.ToInt32(Console.ReadLine());
 
-            if(num1 < 0 || num2 < 0)
+            if (num1 < 0 || num2 < 0)
             {
                 Console.WriteLine("One of two inputs is negative integer");
                 Console.WriteLine(getTotal(num1, num2));
-            } 
-  
+            }
+            else if (num1 > 0 && num2 > 0)
+            {
+                Console.WriteLine("Two inputs are positive integers");
+                Console.WriteLine(getTotal(num1, num2));
+            }
+
+
             else
             {
                 Console.WriteLine(getTotal(num1, num2));
             }
              
-
-
         }
 
         static int getTotal(int num1, int num2)
