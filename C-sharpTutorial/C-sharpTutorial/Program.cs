8a034b510318f337332631d83d1cd07dfe810904
vietcuongk99 <daoquocthai8@gmail.com>
2020-07-01 09:33:38 +0700

    Switch Statement

--- C-sharpTutorial/C-sharpTutorial/Program.cs
+++ C-sharpTutorial/C-sharpTutorial/Program.cs
@@ -7,32 +7,43 @@ namespace C_sharpTutorial
         
         static void Main(string[] args)
         {
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            if (num1 < 0 || num2 < 0)
-            {
-                Console.WriteLine("One of two inputs is negative integer");
-                Console.WriteLine(getTotal(num1, num2));
-            }
-            else if (num1 > 0 && num2 > 0)
-            {
-                Console.WriteLine("Two inputs are positive integers");
-                Console.WriteLine(getTotal(num1, num2));
-            }
-
-
-            else
-            {
-                Console.WriteLine(getTotal(num1, num2));
-            }
-             
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(getDay(num));
         }
 
-        static int getTotal(int num1, int num2)
+        static string getDay(int dayNum)
         {
-            int total = num1 + num2;
-            return total;
+            string dayName;
+            switch(dayNum)
+            {
+             
+                case 1:
+                    dayName = "Sunday";
+                    break;
+                case 2:
+                    dayName = "Monday";
+                    break;
+                case 3:
+                    dayName = "Tuesday";
+                    break;
+                case 4:
+                    dayName = "Wednesday";
+                    break;
+                case 5:
+                    dayName = "Thursday";
+                    break;
+                case 6:
+                    dayName = "Friday";
+                    break;
+                case 7:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+            return dayName;
         }
 
     }
